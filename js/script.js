93ce4763e671d19918b617bc524058950bf961b6
Nijanthan S <nijanthans12@gmail.com>
2020-12-05 17:08:28 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
+
+
 $(document).ready(() => {
 
   $('.hamburger-menu').click(() => {
